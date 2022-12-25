393e7b5ba8a9ff7018a229b50e9adfe6e2aba29e
Shahzod Rahimov <rakhimovsh3235@gmail.com>
2022-12-25 16:58:44 +0500

    updated javascript file

--- weather-app/js/index.js
+++ weather-app/js/index.js
@@ -1,4 +1,4 @@
-const API = "bb88a0ffc7eea11fe67721c822a5f94a";
+const API = "0c1683c24d6ce797bb03e4cb2ded649d";
 const URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API}&units=metric&`;
 
 let seacrh_city = document.querySelector(".seacrh-box");
@@ -9,65 +9,49 @@ let date = document.querySelector(".date");
 let sunrise = document.querySelector(".sunrise");
 let sunset = document.querySelector(".sunset");
 
-date.innerHTML = new Date().toLocaleString("default", {
-  month: "long",
-  day: "numeric",
-  weekday: "long",
-  year: "numeric",
-});
-
 //? Get weather by location
 navigator.geolocation.getCurrentPosition((position) => {
   const { latitude, longitude } = position.coords;
+  getWeather(`lat=${latitude}&lon=${longitude}`);
+});
 
-  fetch(URL + `lat=${latitude}&lon=${longitude}`)
+//? Get weather by input
+seacrh_city.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    getWeather(`q=${seacrh_city.value}`);
+  }
+});
+
+function getWeather(str) {
+  fetch(URL + str)
     .then((response) => response.json())
     .then((data) => {
       try {
+        date.innerHTML = new Date().toLocaleString("default", {
+          month: "long",
+          day: "numeric",
+          weekday: "long",
+          year: "numeric",
+        });
+
         city.innerHTML = `${data.name}, ${data.sys.country}`;
         temp.innerHTML = `${data.main.temp.toFixed()}°C`;
         weather.innerHTML = data.weather[0].main;
 
         sunrise.innerHTML = `sunrise: ${new Date(
           data.sys.sunrise * 1000
-        ).toLocaleString("uz", {
-          timeStyle: "short",
-        })}`;
+        ).toLocaleString("default", { timeStyle: "short" })}`;
 
         sunset.innerHTML = `sunset: ${new Date(
           data.sys.sunset * 1000
-        ).toLocaleString("uz", {
-          timeStyle: "short",
-        })}`;
+        ).toLocaleString("default", { timeStyle: "short" })}`;
+
+        seacrh_city.value = "";
       } catch (error) {
+        if (error.name == "TypeError") {
+          alert("Please enter valid country or city!");
+        }
         console.log(error);
       }
     });
-});
-
-//? Get weather by input
-seacrh_city.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
-    fetch(URL + `q=${seacrh_city.value}`)
-      .then((response) => response.json())
-      .then((data) => {
-        try {
-          city.innerHTML = `${data.name}, ${data.sys.country}`;
-          temp.innerHTML = `${data.main.temp.toFixed()}°C`;
-          weather.innerHTML = data.weather[0].main;
-
-          sunrise.innerHTML = `sunrise: ${new Date(
-            data.sys.sunrise * 1000
-          ).toLocaleString("uz", { timeStyle: "short" })}`;
-
-          sunset.innerHTML = `sunset: ${new Date(
-            data.sys.sunset * 1000
-          ).toLocaleString("uz", { timeStyle: "short" })}`;
-
-          seacrh_city.value = "";
-        } catch (error) {
-          alert("Please enter valid country or city!");
-        }
-      });
-  }
-});
+}
